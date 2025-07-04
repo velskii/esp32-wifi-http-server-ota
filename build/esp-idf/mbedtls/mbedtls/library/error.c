b9d9a97c099fc6ded2976ce36b9c5c8b7abdd05f
velskii <feiliangphp@gmail.com>
2025-07-04 10:48:49 -0400

    Remove build directory from tracking and update .gitignore

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/Users/jerry/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/Users/jerry/esp/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
